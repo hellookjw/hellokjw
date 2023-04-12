12454b23f4ed2d3ba4768638b14b7970aa643429
hellookjw <127330308+hellookjw@users.noreply.github.com>
2023-04-12 23:36:33 +0900

    Create hw5.py

---
+++ hw5.py
@@ -0,0 +1,34 @@
+def read_single_digit(num):
+    if num == 0:
+       return '영'
+    elif num == 1:
+       return '일'
+    elif num == 2:
+       return '이'
+    elif num == 3:
+       return '삼'
+    elif num == 4:
+       return '사'
+    elif num == 5:
+       return '오'
+    elif num == 6:
+       return '육'
+    elif num == 7:
+       return '칠'
+    elif num == 8:
+       return '팔'
+    elif num == 9:
+       return '구'
+
+def read_number(num):
+    hundred = num // 100
+    ten = (num // 10) % 10
+    one = num % 10
+    a = read_single_digit(hundred)
+    b = read_single_digit(ten)
+    c = read_single_digit(one)
+    return a + b + c
+
+num = int(input('세 자리 정수 입력:'))
+result = read_number(num)
+print(result)

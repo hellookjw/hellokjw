9b6d4010949b71d68d4fe2b84aa981f391e432f2
hellookjw <127330308+hellookjw@users.noreply.github.com>
2023-04-20 01:10:46 +0900

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -1,2 +1,7 @@
-def display_multiplication_table(n, k)
-    for i in
+def display_multiplication_table(n, k):
+    for i in range(10, 1):
+        for a in range(n, k, 1):
+            print(f'{i} x {a} = {i * a:2d}')
+            
+display_multiplication_table(2, 6)
+display_multiplication_table(6, 10)

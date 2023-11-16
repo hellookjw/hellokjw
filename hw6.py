55cd18d2a16071b2673e40fb971a2570ec5aac1e
hellookjw <127330308+hellookjw@users.noreply.github.com>
2023-11-17 00:20:16 +0900

    Delete hw6.py

--- hw6.py
+++
@@ -1,7 +0,0 @@
-def display_multiplication_table(n, k):
-    for i in range(10, 1):
-        for a in range(n, k, 1):
-            print(f'{i} x {a} = {i * a:2d}')
-            
-display_multiplication_table(2, 6)
-display_multiplication_table(6, 10)
